c57147f3af3eca898314ea9b13651fe3ac0861fa
Kurimochi <kurimochi.sec@gmail.com>
2026-01-29 18:04:24 +0900

    feat: Add ContentId struct

--- crates/core/src/lib.rs
+++ crates/core/src/lib.rs
@@ -1,1 +1,18 @@
+use std::fmt;
 
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash, Deserialize, Serialize)]
+pub struct ContentId([u8; 32]);
+
+impl From<[u8; 32]> for ContentId {
+    fn from(value: [u8; 32]) -> Self {
+        Self(value)
+    }
+}
+
+impl fmt::Display for ContentId {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", hex::encode(self.0))
+    }
+}
